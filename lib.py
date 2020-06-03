865b1fb1fa589835f1cf2ea60c086a0711493b48
HC im <haechan3@naver.com>
2020-06-03 15:49:27 +0900

    8

---
+++ lib.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def fact(start, end) :
+    f = 1
+    for x in range(start, end + 1) :
+        f *= x
+    return f
+
+def inc_10(x) :
+    x += 10
+    return x
+
+def mul_20(x) :
+    return x*20
+
