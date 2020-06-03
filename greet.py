9ad64507a10ad4dd44b1239ae0007204ae5b4ab4
HC im <haechan3@naver.com>
2020-06-03 09:32:44 +0900

    ex8

---
+++ greet.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def hello1(name) :
+    x = '%s님 안녕하세요.' % name
+    return x
+
+def hello2(name) :
+    x = '%s님 안녕하세요.' % name
+    return x
+
+def hello3(name) :
+    x = '%s님 안녕하세요.' % name
+    return x
+
